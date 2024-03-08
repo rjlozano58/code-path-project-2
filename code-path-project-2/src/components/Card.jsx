fad4acac3eae269932a0adf810762b5404fb3f09
rloza4 <rjlozano58@gmail.com>
2024-03-08 14:49:33 -0600

    pushing changes

---
+++ code-path-project-2/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import React from 'react'
+import '../App.css'
+
+
+const Card = (props) => {
+    return(
+        <div>
+            <h3>Question</h3>
+        </div>
+    )
+}
+
+export default Card;

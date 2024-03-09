9ae1770ecd99f09740a0e03d215eaf92da6078eb
rloza4 <rjlozano58@gmail.com>
2024-03-08 23:21:38 -0600

    added marvel logo

--- code-path-project-2/src/App.jsx
+++ code-path-project-2/src/App.jsx
@@ -1,23 +1,59 @@
 import { useState } from 'react'
 import './App.css'
 import Card from './components/Card.jsx'
+import $ from 'jquery';
+
 
 function App() {
   const [count, setCount] = useState(0);
-  const [randNum, setRand] = useState(0);
+  const [toggle,setToggle] = useState(0);
+  const [currQuestion,setQuestion] = useState(0);
+  const [rand,setRand] = useState(0);
+
+  const data = [
+    ["Who is the super hero that wears a metal suit?","Iron Man"],
+    ["Who is the God of Thunder?","Thor"],
+    ["What super hearo hold the \"Laso of truth\"?","Wonder Woman"],
+    ["Who is Vengeance?","Batman"],
+    ["Who is the super hero that has claws and has super human regeneration?","Wolverine"]
+
+  ]
 
   function generateRandomNum(){
 
   }
+
+  function toggleAnswer(){
+    if (toggle === 0){
+      setToggle(1);
+    }else{
+      setToggle(0);
+    }
+  }
+
+ 
+  function updateRandomNumber(){
+    let oldRand = rand;
+    let newRand = Math.floor(Math.random() * 5);
+    while(oldRand == newRand){
+      newRand = Math.floor(Math.random() * 5);
+    }
+    
+    setRand(newRand);
+  }
+
   return (
+
     <>
-     <h1>Know your super heros!</h1>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/1280px-Marvel_Logo.svg.png"/>
+     <h1 className='title'>TRIVIA</h1>
 
-     <Card />
+        <Card front={data[rand][0]} back={data[rand][1]} />
 
-     <div className='button-container'>
-        <button>Next Question</button>
+      <div className='button-container'>
+        <button onClick={updateRandomNumber}> Next Question</button>
       </div>
+
 
     </>
   )
--- code-path-project-2/src/components/Card.jsx
+++ code-path-project-2/src/components/Card.jsx
@@ -3,11 +3,22 @@ import React from 'react'
 import '../App.css'
 
 
+
 const Card = (props) => {
     return(
-        <div>
-            <h3>Question</h3>
+        <div className='card-flip-container'>
+            <div className='card'>
+                <div className='front-card'>
+                    <h3>{props.front}</h3>
+                </div>
+
+                <div className='back-card'>
+                    <h3>{props.back}</h3>
+                </div>
+            </div>
+
         </div>
+    
     )
 }
 
